a8e08e58aa6ccc182a11bc3413349cda6f400786
benessilfie <bquansah007@gmail.com>
2023-02-06 23:35:26 +0000

    problem set 8: seasons of love

--- problems/2022/p/seasons/seasons.py
+++ problems/2022/p/seasons/seasons.py
@@ -1,11 +1,30 @@
 from datetime import date
+import sys
+import inflect
+
+flect = inflect.engine()
 
 
 def main():
-    ...
+    birthday = input("Enter your birthday (yyyy-mm-dd): ")
+    year, month, day = check_date_format(birthday)
+    birthday = date(int(year), int(month), int(day))
+
+    today = date.today()
+    days = (today - birthday).days
+    minutes = days * 24 * 60
+
+    minutes_in_english = flect.number_to_words(minutes, andword="")
+    print(f"{minutes_in_english.capitalize()} minutes")
 
 
-...
+def check_date_format(birthday):
+    try:
+        year, month, day = birthday.split("-")
+        date(int(year), int(month), int(day))
+        return year, month, day
+    except ValueError:
+        sys.exit("Invalid date")
 
 
 if __name__ == "__main__":
